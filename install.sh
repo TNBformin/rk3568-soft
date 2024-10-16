382fb04577329d7c58aacc8e86a9a4ad94ba8e41
formin <forminlib@gmail.com>
2024-10-16 18:09:28 +0500

    install sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+
+# Файл для уставновки програмнного обеспечения
+# Перед запуском произведите команду: chmod +x install.sh
+# После выполните команду: sh ./install.sh
+
+# Загрузка необходимых библиотек python
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install pyftpdlib
+
+# Отключение старого ftp сервера
+sudo systemctl stop proftpd.service
+sudo systemctl disable proftpd.service
+sudo systemctl status proftpd.service
+
+# Создание собсвенного сервиса для ftp сервера
+# Создание файла службы systemd
+SERVICE_FILE="/etc/systemd/system/pyftpd.service"
+
+echo "[Unit]
+Description=My Python Script Daemon
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 /home/rk3568-soft/ftp-server/ftp.py
+Restart=always
+User=root
+WorkingDirectory=/home/rk3568-soft/ftp-server/
+
+[Install]
+WantedBy=multi-user.target" | sudo tee $SERVICE_FILE
+
+# Перезагрузка конфигурации systemd
+sudo systemctl daemon-reload
+
+# Запуск и включение службы
+sudo systemctl start pyftpd.service
+sudo systemctl enable pyftpd.service
+sudo systemctl status pyftpd.service
